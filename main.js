8b5b9fc15a42f799bde9d3999bdbd06a84f8165a
jwhear93 <jaweebelow420@gmail.com>
2017-06-16 19:00:36 +0100

    Update remover style

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $("#button").click(function () {
         var toAdd = $("input[name=checkListItem]").val();
-        $(".list").append('<div class="item">'+ '<p class="itext">' + 'To-do: ' + '<strong>' + toAdd + '</strong>'+ '<span class="remover">' + ' - Remove' + '</span>' + '</p>' + '<hr>' + '</div>');
+        $(".list").append('<div class="item">'+ '<p class="itext">' + 'To-do: ' + '<strong>' + toAdd + '</strong>'+ ' - ' + '<span class="remover">' + 'X' + '</span>' + '</p>' + '<hr>' + '</div>');
         $(".item").animate({opacity: '1'});
     });
     $(document).on('click', '.remover', function () {
