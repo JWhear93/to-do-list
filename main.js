98210b92d24ddf999a5ab06f8f1b9967cf8686dc
jwhear93 <jaweebelow420@gmail.com>
2017-06-16 16:16:44 +0100

    Add fade-in/out animations for list items

--- main.js
+++ main.js
@@ -2,9 +2,13 @@ $(document).ready(function () {
     $("#button").click(function () {
         var toAdd = $("input[name=checkListItem]").val();
         $(".list").append('<div class="item">'+ '<p class="itext">' + 'To-do: ' + '<strong>' + toAdd + '</strong>'+ '<span class="remover">' + ' - Remove' + '</span>' + '</p>' + '<hr>' + '</div>');
+        $(".item").animate({opacity: '1'});
     });
     $(document).on('click', '.item', function () {
-        $(this).remove();
+    	$(this).fadeOut('medium', function () {
+    		$(this).remove();
+    	});
+        
     });
     $('#clear').click(function () {
     	$('.list').empty();
