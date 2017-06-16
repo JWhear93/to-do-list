8dd1efc709263ed0e2e36adebfc03ac48ca06467
jwhear93 <jaweebelow420@gmail.com>
2017-06-16 19:55:04 +0100

    Add ability to edit tasks + few small style tweaks

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 $(document).ready(function () {
     $("#button").click(function () {
         var toAdd = $("input[name=checkListItem]").val();
-        $(".list").append('<div class="item">'+ '<p class="itext">' + 'To-do: ' + '<strong>' + toAdd + '</strong>'+ ' - ' + '<span class="remover">' + 'X' + '</span>' + '</p>' + '<hr>' + '</div>');
+        $(".list").append('<div class="item">'+ '<p class="itext">' + 'To-do: ' + '<strong>' + '<span class="newText">' + toAdd + '</span>' + '</strong>'+ ' - ' + '<span class="remover">' + 'X' + '</span>' + '</p>' + '<hr>' + '</div>');
         $(".item").animate({opacity: '1'});
     });
     $(document).on('click', '.remover', function () {
     	$(this).parent().fadeOut('medium', function () {
     		$(this).parent().remove();
     	});
-        
+    });
+    $(document).on('click', '.info', function () {
+    	$(this).fadeOut('medium', function () {
+    		$(this).remove();
+    	});
     });
     $('#clear').click(function () {
     	$('.list').empty();
+    });
+    $(document).on('dblclick', '.newText', function () {
+    	var usrInput = prompt('Edit this task', 'Type here');
+    	$(this).text(usrInput);
     });
 });
